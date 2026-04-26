b7cd85bff414edeb229e0644b04793e6987ba494
FjodorWendin <fjodor.wendin@elev.ga.ntig.se>
2026-04-26 22:47:02 +0200

    Idk

--- js/function.js
+++ js/function.js
@@ -1,13 +1,20 @@
 window.onload = main;
 
-function main() {
-    window.addEventListener("scroll", function(){
-        if(isElementIntoView("#start")) document.getElementsByTagName("form")[0].style.opacity = "1" 
-    });
+function isElementIntoView(el) { 
+    let e = document.querySelector(el);
+    let rect = e.getBoundingClientRect();
+        return  window.innerHeight-rect.top >= 200;
 }
 
-function isElementIntoView(el) { 
-    var e = document.querySelector(el);
-    var rect = e.getBoundingClientRect();
-    return  window.innerHeight-rect.top >= 0;     
+function main() {
+    window.addEventListener("scroll", function(){
+        if(isElementIntoView("#bgn")) 
+        document.getElementsByName("begin")[0].style.opacity = "1"
+        if(isElementIntoView("#inf1")) 
+        document.getElementsByName("info1")[0].style.opacity = "1"
+        if(isElementIntoView("#inf2")) 
+        document.getElementsByName("info2")[0].style.opacity = "1"
+        if(isElementIntoView("#start")) 
+        document.getElementsByName("start-button")[0].style.opacity = "1"
+    });
 }
